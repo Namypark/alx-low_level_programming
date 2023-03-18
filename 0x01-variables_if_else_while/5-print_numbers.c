5bfd3afd2cd7f1b6c3e2137b25bff16e2258eb59
Namy <namy_park@protonmail.com>
2023-03-17 21:31:54 -0700

    added 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function takes an integer input from the user and checks whether it is
+ * positive or negative. It then prints an appropriate message to the console
+ * indicating the result.
+ *
+ * Return: 0 if the program runs successfully
+ */
+int main(void)
+{
+    int i;
+    for (i=0; i < 10; i++)
+	{
+	    printf("%d", i);
+	}
+    printf("\n");
+    return 0;
+}
+
