15d9b30762fd62ab7430967c7d9f7155d23d3186
__Namy__ <40345364+Namypark@users.noreply.github.com>
2023-03-18 23:39:01 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,11 +4,20 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point for the program
+ *
+ * This function takes an integer input from the user and checks whether it is
+ * positive or negative. It then prints an appropriate message to the console
+ * indicating the result.
+ *
+ * Return: 0 if the program runs successfully
+ */
 int main(void)
 {
 	int n;
 	int last_digit;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
