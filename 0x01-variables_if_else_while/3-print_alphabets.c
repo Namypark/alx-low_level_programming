6f4b3f3a16e10f22a3d91c88bc353d5bd4782c97
Namy <namy_park@protonmail.com>
2023-03-17 20:47:33 -0700

    Added 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * This function takes an integer input from the user and checks whether it is
+ * positive or negative. It then prints an appropriate message to the console
+ * indicating the result.
+ *
+ * Return: 0 if the program runs successfully
+ */
+int main() {
+    char letter = 'a'; 
+    int i;
+    
+    
+    for (i = 0; i < 26; i++) 
+    {
+        putchar(letter);
+        letter++; 
+    }
+    
+    letter = 'A';
+    
+   
+    for (i = 0; i < 26; i++) 
+    {
+        putchar(letter);
+        letter++; 
+    }
+    
+    putchar('\n'); 
+    
+    return 0;
+}
+
