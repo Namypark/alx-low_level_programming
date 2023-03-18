6426bdb44de44e3c2d000984d28510e6830ffe62
__Namy__ <40345364+Namypark@users.noreply.github.com>
2023-03-18 03:36:24 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,15 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-
-/* Description: program takes an integer  */
+/**
+ * main - Entry point for the program
+ *
+ * This function takes an integer input from the user and checks whether it is
+ * positive or negative. It then prints an appropriate message to the console
+ * indicating the result.
+ *
+ * Return: 0 if the program runs successfully
+ */
 int main(void)
 {
 	int n;
