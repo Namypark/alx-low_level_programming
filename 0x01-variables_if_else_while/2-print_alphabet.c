8f359b9f620cee878c2f5306dc21d077c8b97073
__Namy__ <40345364+Namypark@users.noreply.github.com>
2023-03-18 23:24:33 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,20 +9,16 @@
  *
  * Return: 0 if the program runs successfully
  */
-int main()
+int main(void)
 {
     char letter = 'a';
     int i;
-    
-    
     for (i = 0; i < 26; i++)
     {
         putchar(letter);
         letter++;
     }
-    
     putchar('\n');
-
     return 0;
 }
 
