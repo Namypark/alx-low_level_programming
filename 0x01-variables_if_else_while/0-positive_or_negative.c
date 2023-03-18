b6e92a4765acb836f347a88029c560294c799587
__Namy__ <40345364+Namypark@users.noreply.github.com>
2023-03-18 03:25:29 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
-	 * This is the preferred style for multi-line
-	 * comments in C source code.
-	 * Please use it consistently.
-	 *
 	 * Description:  A column of asterisks on the left side,
-	 * with beginning and ending almost-blank lines.
 	 */
 int main(void)
 {
