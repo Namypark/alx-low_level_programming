c3000fe84c3503367a4684180f8d404261732192
Namy <namy_park@protonmail.com>
2023-03-27 17:24:35 -0700

    Added file 0-11

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 = updates the value of a variable
+ * the pointer will point to 98
+ * @n -> pointer to the variable to update
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "#main.h"
+/**
+ * swap_int - swaps value of two integers
+ * @a: pointer of first value
+ * @b: pointer of second value
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
